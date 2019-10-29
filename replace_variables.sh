0e0ec77993ba60614ce79de6d463e6216d171591
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2019-10-29 16:26:05 -0400

    adding api management secret

--- replace_variables.sh
+++ replace_variables.sh
@@ -1,9 +1,9 @@
-# [ -z "${SECRET}" ] && echo "Not set: SECRET";
+[ -z "${PRISMA_ADMIN_SECRET}" ] && echo "Not set: PRISMA_ADMIN_SECRET";
 [ -z "${PRISMA_DB_HOST}" ] && echo "Not set: DATABASE_HOST";
 [ -z "${PRISMA_DB_USER}" ] && echo "Not set: DATABASE_USER";
 [ -z "${PRISMA_DB_PASSWORD}" ] && echo "Not set: DATABASE_PASSWORD";
 
-# sed -ri 's/managementApiSecret: SECRET/managementApiSecret: '$SECRET'/g' prisma.yml
+sed -ri 's/managementApiSecret: PRISMA_ADMIN_SECRET/managementApiSecret: '$PRISMA_ADMIN_SECRET'/g' prisma.yml
 sed -ri 's/host: PRISMA_DB_HOST/host: '$PRISMA_DB_HOST'/g' prisma.yml
 sed -ri 's/user: PRISMA_DB_USER/user: '$PRISMA_DB_USER'/g' prisma.yml
 sed -ri 's/password: PRISMA_DB_PASSWORD/password: '$PRISMA_DB_PASSWORD'/g' prisma.yml
